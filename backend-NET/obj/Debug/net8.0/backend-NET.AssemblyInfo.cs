3e02c1c80e1006f581fc23acbcc171a58c9b269e
RapaxAgrum <jaynarainlauralee@gmail.com>
2026-04-22 15:18:47 +0200

    backend files untracked

--- backend-NET/obj/Debug/net8.0/backend-NET.AssemblyInfo.cs
+++ backend-NET/obj/Debug/net8.0/backend-NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5bbbe53cb7466d836c5790ce532fdf4d657c6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc1f1862d73de0ccdc5027295ef9a313ae75ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
